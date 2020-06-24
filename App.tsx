9c3e4925d7107a50fdaff908281261f1c33f1e88
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-06-24 03:25:45 -0500

    styled components + vector-icons update

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
+import Main from "./src/pages/Main";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <React.Fragment>
+      <Main />
+    </React.Fragment>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
